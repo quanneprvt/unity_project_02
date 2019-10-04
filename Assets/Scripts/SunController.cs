0f5ebbae95696f04ce388ff617d203ee46a10c96
Nguyen Ba Quan <quanneprvt@gmail.com>
2019-10-04 18:11:22 +0700

    + Sun Behavior

---
+++ Assets/Scripts/SunController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunController : MonoBehaviour
+{
+    //Vector to rotate 60,2,0
+    [SerializeField] private Vector3 m_RotateAxis;
+    [SerializeField] private float m_Speed;
+    private float m_Step = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // m_Step += m_Speed * Time.deltaTime;
+        transform.RotateAround(m_RotateAxis, transform.position, m_Speed/180 * (float)Math.PI);
+    }
+}
