bf274ad6fadd96f14729a9da9781f079df9a54cf
Nguyen Ba Quan <quanneprvt@gmail.com>
2019-10-07 00:32:19 +0700

    + New File Structure, Add some script Mgr

---
+++ Assets/Scripts/GameMgr.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    [SerializeField] private LevelMgr LevelManager;
+    private static GameMgr instance  = null;
+    private enum State {INIT, PLAY, OVER};
+    private State m_State;
+    void Awake()
+    {
+        _SetState(State.INIT);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (m_State)
+        {
+            case State.PLAY:
+                if (LevelManager.IsPass())
+                    _NextLevel();
+            break;
+
+            case State.OVER:
+            break;
+        }
+    }
+
+    private void _NextLevel()
+    {
+        LevelManager.NextLevel();
+    }
+
+    private void _SetState(State s)
+    {
+        m_State = s;
+        switch (m_State)
+        {
+            case State.INIT:
+                // Singleton
+                if (instance == null)
+                    instance = this;
+                else if (instance != this)
+                    Destroy(gameObject);
+                //
+                DontDestroyOnLoad(gameObject);
+                //
+                LevelManager.Init();
+                //
+                _SetState(State.PLAY);
+            break;
+
+            case State.PLAY:
+                LevelManager.Start();
+            break;
+
+            case State.OVER:
+            break;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelDefine/LevelMgr.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMgr : MonoBehaviour
+{
+    [SerializeField] private int m_CurrentLevelIndex = 0;
+    [SerializeField] private LevelDefine m_Levels;
+    private static LevelMgr instance = null;
+    private enum State { INIT, PLAY};
+    private State m_State;
+    private Level m_CurrentLevel;
+    private bool m_IsPass = false;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        switch (m_State)
+        {
+            case State.INIT:
+                if (m_CurrentLevel.IsPass())
+                    m_IsPass = true;
+            break;
+
+            case State.PLAY:
+            break;
+        }
+    }
+
+    public void Init()
+    {
+        _SetState(State.INIT);
+    }
+
+    public bool IsPass()
+    {
+        return m_IsPass;
+    }
+
+    public void NextLevel()
+    {
+        _SetLevel(m_CurrentLevelIndex += 1);
+    }
+
+    private void _SetState(State s)
+    {
+        m_State = s;
+        switch (m_State)
+        {
+            case State.INIT:
+                // Singleton
+                if (instance == null)
+                {
+                    instance = this;
+                    _SetLevel(m_CurrentLevelIndex);
+                }
+                //
+            break;
+
+            case State.PLAY:
+            break;
+        }
+    }
+
+    private void _SetLevel(int l)
+    {
+        if (m_CurrentLevelIndex >= m_Levels.Length)
+        {
+            Time.timeScale = 0;
+            return;
+        }
+        m_CurrentLevel = m_Levels[l];
+    }
+}
